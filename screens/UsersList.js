f98499103716657bbacb276c23b8f56c28efc521
Jocalu <jocalu87@gmail.com>
2021-04-12 12:05:42 +0200

    Add userlist

--- screens/UsersList.js
+++ screens/UsersList.js
@@ -1,21 +1,35 @@
+/* eslint-disable react/prop-types */
 import React , {useEffect, useState} from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, Button} from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 import firebase from '../database/firebase'
 
-const UsersList = () => {
+const UsersList = (props) => {
   const [users, setUsers] = useState([])
 
   useEffect(() => {
     firebase.db.collection('users').onSnapshot(querySnapshot => {
+      const users = [];
+
       querySnapshot.docs.forEach(doc => {
-        console.log(doc.data())
+        const {name, email, phone} = doc.data()
+        users.push({
+          id: doc.id,  
+          name, 
+          email, 
+          phone
+        })
       })
+      setUsers(users)
     })
-  })
+  }), ([]);
+
   return (
-    <View>
-      <Text>Users List</Text>
-    </View>
+   <ScrollView>
+     <Button 
+      title="Create User" 
+      onPress={() => props.navigation.navigate('CreateUserScreen')}></Button>
+   </ScrollView>
   )
 }
 export default UsersList
