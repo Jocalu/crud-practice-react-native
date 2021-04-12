95736065ceb1c6fd67c532ba358ac6e1e0a4b838
Jocalu <jocalu87@gmail.com>
2021-04-12 11:00:39 +0200

    Add dependecies

--- App.js
+++ App.js
@@ -2,10 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+function myStack() {
+  return (
+
+  )
+}
 
 export default function App() {
   return (
+<NavigationContainer>
 
+</NavigationContainer>
   );
 }
 
